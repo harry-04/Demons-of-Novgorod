3b05aa743cbf53535e61b8b91921be83daa42d03
harry-04 <72386526+harry-04@users.noreply.github.com>
2022-05-19 13:58:41 +0100

    PirateDenBegins

---
+++ Demons of Novgorod/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider col)
+    {
+        col.gameObject.GetComponent<PlayerHealth>().Die();
+    }
+}
